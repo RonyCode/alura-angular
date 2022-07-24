b5f0c8888c9f38c4a3b925fb29664c0d313d24c3
ronycode <ronypc@outlook.com>
2022-07-24 12:15:10 -0300

    curso angular pelo desktop

---
+++ apps/estudo-angular/src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './home/signin/signin.component';
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
+import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
+import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { AuthGuard } from './core/auth/auth.guard';
+import { SignupComponent } from './home/signup/signup.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SignInComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+  {
+    path: 'user/:userName',
+    component: PhotoListComponent,
+    resolve: {
+      photos: PhotoListResolver,
+    },
+  },
+  {
+    path: 'p/add',
+    component: PhotoFormComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ apps/estudo-angular/src/app/home/home.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { SignInComponent } from './signin/signin.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import { RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+
+@NgModule({
+  declarations: [SignInComponent, SignupComponent],
+  imports: [CommonModule, ReactiveFormsModule, VMessageModule, RouterModule],
+})
+export class HomeModule {}
