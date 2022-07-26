d43637e0c88f74c8c6483ea1b27dc0ef39907a7a
ronycode <ronypc@outlook.com>
2022-07-25 22:25:29 -0300

    lazy load

--- apps/estudo-angular/src/app/home/signup/signup.component.ts
+++ apps/estudo-angular/src/app/home/signup/signup.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { lowerCaseValidator } from '../../shared/validators/validator/lower-case-validator';
+import { UserNotTakenValidatorService } from './user-not-taken-validator.service';
+import { NewUser } from './new-user';
+import { SignUpService } from './signup.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ap-signup',
@@ -10,9 +15,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SignupComponent implements OnInit {
   signForm: FormGroup | any;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private checkUserTaken: UserNotTakenValidatorService,
+    private signUpService: SignUpService,
+    private route: Router,
+    private elementDom: Renderer2
+  ) {}
 
   ngOnInit() {
+    this.elementDom.selectRootElement('#signUpFocus').focus();
+
     this.signForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
 
@@ -29,21 +42,31 @@ export class SignupComponent implements OnInit {
         '',
         [
           Validators.required,
-          Validators.pattern(/^[a-z\d_\-]+$/),
+          lowerCaseValidator,
           Validators.minLength(2),
           Validators.maxLength(30),
         ],
+        this.checkUserTaken.checkUsernameTaken(),
       ],
 
       password: [
         '',
         [
           Validators.required,
-          Validators.pattern(/^[a-z\d_\-]+$/),
-          Validators.minLength(2),
-          Validators.maxLength(30),
+          Validators.minLength(8),
+          Validators.maxLength(14),
         ],
       ],
+    });
+  }
+
+  signUp() {
+    const newUser = this.signForm.getRawValue() as NewUser;
+    this.signUpService.signUp(newUser).subscribe({
+      next: () => {
+        this.route.navigate(['']);
+      },
+      error: (err) => console.log(err),
     });
   }
 }
