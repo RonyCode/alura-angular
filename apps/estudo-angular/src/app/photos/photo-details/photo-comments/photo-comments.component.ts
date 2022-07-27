f4cccd285b5ad33711e72a9221d37dcdad00efa6
rony <ronypc@outlook.com>
2022-07-27 12:56:43 -0300

    recuperando pelo note

---
+++ apps/estudo-angular/src/app/photos/photo-details/photo-comments/photo-comments.component.ts
@@ -0,0 +1,43 @@
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { PhotoService } from '../../photo/photo.service';
+import { Observable, switchMap } from 'rxjs';
+import { PhotoComment } from '../../photo/photo-comment';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { tap } from 'rxjs/operators';
+
+@Component({
+  selector: 'ap-photo-comments',
+  templateUrl: './photo-comments.component.html',
+  styleUrls: ['./photo-comments.component.css'],
+  encapsulation: ViewEncapsulation.Emulated,
+})
+export class PhotoCommentsComponent implements OnInit {
+  @Input() photoId!: number;
+  comments$!: Observable<PhotoComment[]>;
+  commentForm: FormGroup | any;
+
+  constructor(
+    private photoService: PhotoService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.comments$ = this.photoService.getComments(this.photoId);
+    this.commentForm = this.formBuilder.group({
+      comment: ['', Validators.maxLength(300)],
+    });
+  }
+
+  save() {
+    const comment = this.commentForm.get('comment').value as string;
+    this.comments$ = this.photoService
+      .addComment(this.photoId, comment)
+      .pipe(switchMap(() => this.photoService.getComments(this.photoId)))
+      .pipe(
+        tap(() => {
+          this.commentForm.reset();
+          alert('Comentario inserido com sucesso');
+        })
+      );
+  }
+}
